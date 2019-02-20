4f9904a6c8c718e1825c0ac0c009083f6996de43
Sadiya3897 <khansadiya397@gmail.com>
2019-02-20 15:40:37 +0530

    Add files via upload

---
+++ Addition.js
@@ -0,0 +1,12 @@
+function add(x) {
+    return function (y) {
+        if (typeof y !== 'undefined') {
+            x = x + y;
+            return arguments.callee;
+        } else {
+            return x;
+        }
+    };
+
+}
+console.log(add(1)(2)());
